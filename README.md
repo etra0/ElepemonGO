3f3572b1e75da490265cee58eab443af33084827
Etra <saedo@alumnos.inf.utfsm.cl>
2016-09-06 19:12:53 -0300

    First commit, with a test in it

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tarea 1 LP
+**Integrantes**
+- Sebastián Aedo (201573057-1)
+- Gaspar Correa (201573009-1)
+
+Para compilar el código correctamente, necesitamos [**inih**](https://github.com/benhoyt/inih) en la carpeta raíz del código.
---
+++ attacks/attack.h
@@ -0,0 +1,14 @@
+#ifndef ATTACK_H
+#define ATTACK_H
+
+#include <elepemon.h>
+
+/*
+* Firma de funcion que cualquier ataque debiera implementar
+* @param 1 atacante
+* @param 2 defensor
+* @return el resultado del ataque, alojado en el heap. debe ser liberado
+*/
+struct atack_result* attack(struct elepemon*, struct elepemon*);
+
+#endif // ATTACK_H
