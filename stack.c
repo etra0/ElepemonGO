108d637c75fec24b08aefc4ba554757eb37cb031
Gaspar Correa <gasparcorreavergara@gmail.com>
2016-09-14 15:54:26 -0300

    Se añadieron funciones a stack.c

--- stack.c
+++ stack.c
@@ -5,6 +5,32 @@ int is_empty(struct elepemon_node* stack)
     return stack == NULL;
 }
 
+struct elepemon* get_elepemon( struct elepemon_node* stack, const char* name)
+{
+    struct elepemon_node* ptr = stack;
+
+    while( !(is_empty (ptr)) ){
+        if ( ptr -> elepemon.name == name ){
+            return elepemon;
+        }
+        ptr = ptr -> next;
+    }
+
+    return NULL;
+}
+
+void free_elepemon_stack(struct elepemon_node* stack)
+{
+    while( !(is_empty(stack)) ){
+        struct elepemon_node* aux;
+        free_elepemon( stack->elepemon );
+        aux = stack;
+        stack = stack-> next;
+        free(aux);
+    }
+
+}
+
 struct elepemon_node* push_elepemon(struct elepemon_node** stack, const char* name)
 {
     struct elepemon_node* aux;
