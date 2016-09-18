353e53ef65c12cc650fe5aa15f5fc5f289f2dc56
Gaspar Correa <gasparcorreavergara@gmail.com>
2016-09-18 17:59:02 -0300

    Corregi los ataques

--- attacks/plant1.c
+++ attacks/plant1.c
@@ -11,7 +11,12 @@ struct attack_result* attack(struct elepemon* attacker, struct elepemon* defenso
     attack_info->effect = 2;
     power = attacker->power;
     defense = defensor->defense;
-    standard_damage = (power/2)-(defense/3);
+    
+    if (power > defense){
+        standard_damage = (power/2)-(defense/3);
+    }else{
+        standard_damage = (power/2);
+    }
 
     if (defensor->type == 0) {
         attack_info->effective = 3;
